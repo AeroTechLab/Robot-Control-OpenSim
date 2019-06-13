7d5db8be81ee6dcf724a900aa2047923f1772954
Bitiquinho <consoni_2519@hotmail.com>
2019-06-13 16:46:11 -0300

    IK control plugin WIP initial attempt

--- osim_model-ik.cpp
+++ osim_model-ik.cpp
@@ -6,7 +6,7 @@
 
 #include <iostream>
 #include <string>
-//#include <vector>
+#include <vector>
 
 #include "interface/robot_control.h"
 
@@ -19,7 +19,7 @@
 struct
 {
   OpenSim::Model* osimModel;
-  SimTK::State stateFD, stateIK;
+  SimTK::State state;
   std::vector<OpenSim::CoordinateActuator*> actuatorsList;
   std::vector<int> accelerationIndexesList;
   SimTK::Array_<OpenSim::CoordinateReference> coordinateReferences;
@@ -27,7 +27,6 @@ struct
   OpenSim::MarkersReference markersReference;
   OpenSim::Set<OpenSim::MarkerWeight> markerWeights;
   std::vector<std::string> markerLabels;
-  std::vector<SimTK::Vec3> markerSetpoints;
   std::vector<SimTK::Vec3> markerInitialLocations;
   SimTK::Matrix_<SimTK::Vec3> markerSetpointsTable;
   std::vector<char*> jointNamesList;
@@ -41,6 +40,8 @@ controller;
 
 DECLARE_MODULE_INTERFACE( ROBOT_CONTROL_INTERFACE );
 
+
+const size_t VEC3_SIZE = SimTK::Vec3::size();
 
 bool InitController( const char* data )
 { 
@@ -91,35 +92,31 @@ bool InitController( const char* data )
       }
     }
     std::cout << "OpenSim model loaded successfully ! (" << controller.osimModel->getNumCoordinates() << " coordinates)" << std::endl;
-    SimTK::State& stateFD = controller.osimModel->initializeState();    // Initialize the system
+    controller.state = controller.osimModel->initializeState();    // Initialize the system
     for( int muscleIndex = 0; muscleIndex < muscleSet.getSize(); muscleIndex++ )
 #ifdef OSIM_LEGACY
-      muscleSet[ muscleIndex ].setDisabled( stateFD, true );
+      muscleSet[ muscleIndex ].setDisabled( controller.state, true );
 #else
-      muscleSet[ muscleIndex ].setAppliesForce( stateFD, false );
+      muscleSet[ muscleIndex ].setAppliesForce( controller.state, false );
 #endif
     for( size_t jointIndex = 0; jointIndex < controller.actuatorsList.size(); jointIndex++ )
     {
 #ifdef OSIM_LEGACY
-      controller.actuatorsList[ jointIndex ]->overrideForce( stateFD, true );
+      controller.actuatorsList[ jointIndex ]->overrideForce( controller.state, true );
 #else
-      controller.actuatorsList[ jointIndex ]->overrideActuation( stateFD, true );
+      controller.actuatorsList[ jointIndex ]->overrideActuation( controller.state, true );
 #endif
-      controller.actuatorsList[ jointIndex ]->getCoordinate()->setValue( stateFD, 0.0 );
+      controller.actuatorsList[ jointIndex ]->getCoordinate()->setValue( controller.state, 0.0 );
     }
     
-    controller.markerSetpoints.resize( controller.markers.getSize(), SimTK::Vec3( 0.0 ) );
     controller.markerInitialLocations.resize( controller.markers.getSize(), SimTK::Vec3( 0.0 ) );
     controller.markerSetpointsTable.resize( 1, controller.markers.getSize() );
     for( int markerIndex = 0; markerIndex < controller.markers.getSize(); markerIndex++ )
 #ifdef OSIM_LEGACY
-      controller.osimModel->getSimbodyEngine().getPosition( controller.stateFD, controller.markers[ markerIndex ].getBody(), controller.markers[ markerIndex ].getOffset(), controller.markerInitialLocations[ markerIndex ] );
+      controller.osimModel->getSimbodyEngine().getPosition( controller.state, controller.markers[ markerIndex ].getBody(), controller.markers[ markerIndex ].getOffset(), controller.markerInitialLocations[ markerIndex ] );
 #else
-      controller.markerInitialLocations[ markerIndex ] = controller.markers[ markerIndex ].getLocationInGround( stateFD );
+      controller.markerInitialLocations[ markerIndex ] = controller.markers[ markerIndex ].getLocationInGround( controller.state );
 #endif
-    
-    controller.stateFD.setTime( 0.0 );
-    controller.stateIK = controller.stateFD;
     
     controller.emgProcessor = new EMGOptimizerImpl( *(controller.osimModel), controller.actuatorsList, 1000 );
     
@@ -182,14 +179,14 @@ void GetExtraOutputsList( double* outputsList ) { }
 
 void SetControlState( enum RobotState newControlState )
 { 
-  std::cout << "setting new control stateFD: " << newControlState;
+  std::cout << "setting new control state: " << newControlState;
 
   const OpenSim::ForceSet &forceSet = controller.osimModel->getForceSet();
   for( int forceIndex = 0; forceIndex < forceSet.getSize(); forceIndex++ )
 #ifdef OSIM_LEGACY
-    forceSet[ forceIndex ].setDisabled( controller.stateFD, true );
+    forceSet[ forceIndex ].setDisabled( controller.state, true );
 #else
-    forceSet[ forceIndex ].setAppliesForce( controller.stateFD, false );
+    forceSet[ forceIndex ].setAppliesForce( controller.state, false );
 #endif
 
   if( newControlState == ROBOT_OFFSET )
@@ -224,9 +221,9 @@ void SetControlState( enum RobotState newControlState )
 
         for( int forceIndex = 0; forceIndex < forceSet.getSize(); forceIndex++ )
 #ifdef OSIM_LEGACY
-          forceSet[ forceIndex ].setDisabled( controller.stateFD, false );
+          forceSet[ forceIndex ].setDisabled( controller.state, false );
 #else
-          forceSet[ forceIndex ].setAppliesForce( controller.stateFD, true );
+          forceSet[ forceIndex ].setAppliesForce( controller.state, true );
 #endif
       }
     }
@@ -245,20 +242,20 @@ void PreProcessSample( SimTK::Vector& inputSample, SimTK::Vector& outputSample )
   {
     OpenSim::Coordinate* jointCoordinate = controller.actuatorsList[ jointIndex ]->getCoordinate();
     int actuatorInputsIndex = controller.emgInputs.size() + jointIndex * EMG_INPUT_VARS_NUMBER;
-    jointCoordinate->setValue( controller.stateFD, inputSample[ actuatorInputsIndex + EMG_POSITION ] );
-    jointCoordinate->setSpeedValue( controller.stateFD, inputSample[ actuatorInputsIndex + EMG_VELOCITY ] );
+    jointCoordinate->setValue( controller.state, inputSample[ actuatorInputsIndex + EMG_POSITION ] );
+    jointCoordinate->setSpeedValue( controller.state, inputSample[ actuatorInputsIndex + EMG_VELOCITY ] );
     int jointAccelerationIndex = controller.accelerationIndexesList[ jointIndex ];
     accelerationsList[ jointAccelerationIndex ] = inputSample[ actuatorInputsIndex + EMG_ACCELERATION ];
 #ifdef OSIM_LEGACY
-    controller.actuatorsList[ jointIndex ]->setOverrideForce( controller.stateFD, inputSample[ actuatorInputsIndex + EMG_TORQUE_EXT ] );
+    controller.actuatorsList[ jointIndex ]->setOverrideForce( controller.state, inputSample[ actuatorInputsIndex + EMG_TORQUE_EXT ] );
 #else
-    controller.actuatorsList[ jointIndex ]->setOverrideActuation( controller.stateFD, inputSample[ actuatorInputsIndex + EMG_TORQUE_EXT ] );
+    controller.actuatorsList[ jointIndex ]->setOverrideActuation( controller.state, inputSample[ actuatorInputsIndex + EMG_TORQUE_EXT ] );
 #endif
   }
   
   try
   {
-    SimTK::Vector idForcesList = idSolver.solve( controller.stateFD, accelerationsList );
+    SimTK::Vector idForcesList = idSolver.solve( controller.state, accelerationsList );
     
     for( int jointIndex = 0; jointIndex < controller.actuatorsList.size(); jointIndex++ )
     {
@@ -283,8 +280,8 @@ void PreProcessSample( SimTK::Vector& inputSample, SimTK::Vector& outputSample )
 
 void RunControlStep( RobotVariables** jointMeasuresList, RobotVariables** axisMeasuresList, RobotVariables** jointSetpointsList, RobotVariables** axisSetpointsList, double timeDelta )
 {
-  controller.stateFD.updTime() = 0.0;
-
+  controller.state.setTime( 0.0 );
+  // Acquire training/optimization samples
   SimTK::Vector actuatorInputs( EMG_INPUT_VARS_NUMBER * controller.actuatorsList.size() );
   SimTK::Vector actuatorOutputs( EMG_OUTPUT_VARS_NUMBER * controller.actuatorsList.size() );
   for( size_t jointIndex = 0; jointIndex < controller.actuatorsList.size(); jointIndex++ )
@@ -296,69 +293,62 @@ void RunControlStep( RobotVariables** jointMeasuresList, RobotVariables** axisMe
     actuatorInputs[ actuatorInputsIndex + EMG_SETPOINT ] = jointMeasuresList[ jointIndex ]->acceleration;
     actuatorInputs[ actuatorInputsIndex + EMG_TORQUE_EXT ] = jointMeasuresList[ jointIndex ]->force;
   }
-  
+  // Calculate additional samples
   PreProcessSample( actuatorInputs, actuatorOutputs );
-  
+  // Store samples for training/optimization or calculating outputs
   if( controller.currentControlState == ROBOT_PREPROCESSING )
     controller.emgProcessor->StoreSamples( controller.emgInputs, actuatorInputs, actuatorOutputs );
   else if( controller.currentControlState == ROBOT_OPERATION )
     actuatorOutputs = controller.emgProcessor->CalculateOutputs( controller.emgInputs, actuatorInputs );
-  
+  // Set joint state measurements for forward kinematics/dynamics
   for( size_t jointIndex = 0; jointIndex < controller.actuatorsList.size(); jointIndex++ )
   {
     OpenSim::Coordinate* jointCoordinate = controller.actuatorsList[ jointIndex ]->getCoordinate();
-    jointCoordinate->setValue( controller.stateFD, jointMeasuresList[ jointIndex ]->position );
-    jointCoordinate->setSpeedValue( controller.stateFD, jointMeasuresList[ jointIndex ]->velocity );
+    jointCoordinate->setValue( controller.state, jointMeasuresList[ jointIndex ]->position );
+    jointCoordinate->setSpeedValue( controller.state, jointMeasuresList[ jointIndex ]->velocity );
     size_t actuatorOutputsIndex = jointIndex * EMG_OUTPUT_VARS_NUMBER;
     double resultingTorque = jointMeasuresList[ jointIndex ]->force + actuatorOutputs[ actuatorOutputsIndex + EMG_TORQUE_INT ];
-    controller.actuatorsList[ jointIndex ]->setOverrideActuation( controller.stateFD, resultingTorque );
+    controller.actuatorsList[ jointIndex ]->setOverrideActuation( controller.state, resultingTorque );
   }
-  
-//   OpenSim::Manager manager( *(controller.osimModel) );
-// #ifdef OSIM_LEGACY
-//   manager.integrate( controller.stateFD, timeDelta );
-// #else
-//   controller.stateFD = manager.integrate( timeDelta );
-// #endif
-  
-  OpenSim::TimeSeriesTableVec3 markersTimeTable( std::vector<double>( { 0.0 } ) );
-  SimTK::RowVector_<SimTK::Vec3> markersTimeRow( controller.markers.getSize() );
+  // Calculate resulting model state
+  OpenSim::Manager manager( *(controller.osimModel) );
+#ifdef OSIM_LEGACY
+  manager.integrate( controller.state, timeDelta );
+#else
+  controller.state = manager.integrate( timeDelta );
+#endif
+  // Iterate over translation/axis markers
+  std::vector<SimTK::Vec3> markerSetpoints;
   for( int markerIndex = 0; markerIndex < controller.markers.getSize(); markerIndex++ )
   {
-    size_t markerAxisIndex = controller.markerAxes[ markerIndex ];
-    axisMeasuresList[ markerIndex ]->position = controller.markers[ markerIndex ].getLocationInGround( controller.stateFD )[ markerAxisIndex ];
-    axisMeasuresList[ markerIndex ]->velocity = controller.markers[ markerIndex ].getVelocityInGround( controller.stateFD )[ markerAxisIndex ];
-    axisMeasuresList[ markerIndex ]->acceleration = controller.markers[ markerIndex ].getAccelerationInGround( controller.stateFD )[ markerAxisIndex ];
-    //axisMeasuresTable[ markerIndex ]->force = jointMeasuresTable[ markerIndex ]->force;
-    
-    markersTimeRow[ markerIndex ][ markerAxisIndex ] = axisSetpointsList[ markerIndex ]->position;
+    for( size_t axisIndex = 0; axisIndex < 3; axisIndex++ )
+    {
+      size_t markerAxisIndex = 3 * markerIndex + axisIndex;
+      // Acquire translation/axis measurements 
+      axisMeasuresList[ markerAxisIndex ]->position = controller.markers[ markerIndex ].getLocationInGround( controller.state )[ axisIndex ];
+      axisMeasuresList[ markerAxisIndex ]->velocity = controller.markers[ markerIndex ].getVelocityInGround( controller.state )[ axisIndex ];
+      axisMeasuresList[ markerAxisIndex ]->acceleration = controller.markers[ markerIndex ].getAccelerationInGround( controller.state )[ axisIndex ];
+      // Set translation/axis setpoints for inverse kinematics
+      markerSetpoints[ markerIndex ][ axisIndex ] = axisSetpointsList[ markerAxisIndex ]->position;
+    }
+    controller.markerSetpointsTable.set( 0, markerIndex, controller.markerInitialLocations[ markerIndex ] + markerSetpoints[ markerIndex ] );
   }
-  markersTimeTable.setRow( 0.0, markersTimeRow );
+  // Setup and run inverse kinematics solver 
+  OpenSim::TimeSeriesTableVec3 markersTimeTable( std::vector<double>( { 0.0 } ), controller.markerSetpointsTable, controller.markerLabels );
   OpenSim::MarkersReference markersReference( markersTimeTable, &(controller.markerWeights) );
-  OpenSim::InverseKinematicsSolver ikSolver( *(controller.osimModel), markersReference, controller.coordinateReferences );
-  ikSolver.setAccuracy( 1.0e-4 ); std::cout << "OSim: IK solver set up" << std::endl;
-  ikSolver.assemble( controller.stateFD ); std::cout << "OSim: IK solver assembled" << std::endl;
-  ikSolver.track( controller.stateFD );
-  
-  OpenSim::TimeSeriesTableVec3 markersTimeTable( std::vector<double>( { ikState.getTime() } ), markersTable, controller.markerLabels );
-  OpenSim::MarkersReference markersReference( markersTimeTable, &(controller.markerWeights) );
-  OpenSim::InverseKinematicsSolver ikSolver( osimModel, markersReference, coordinateReferences, 0.0 );
+  OpenSim::InverseKinematicsSolver ikSolver( *(controller.osimModel), markersReference, controller.coordinateReferences, 0.0 );
   ikSolver.setAccuracy( 1.0e-4 ); //std::cout << "OSim: IK solver set up" << std::endl;
-  ikSolver.assemble( ikState ); //std::cout << "OSim: IK solver assembled" << std::endl;
-  ikSolver.track( ikState );
-  for( size_t jointIndex = 0; jointIndex < controller.actuatorsList.size(); jointIndex++ )
-  {
-    OpenSim::Coordinate* jointCoordinate = actuatorsList[ jointIndex ]->getCoordinate();
-    jointCoordinate->setValue( state, jointCoordinate->getValue( ikState ) );
-    //jointCoordinate->setSpeedValue( state, jointCoordinate->getSpeedValue( ikState ) );
-    //jointCoordinate->setAccelerationValue( state, jointCoordinate->getAccelerationValue( ikState ) );
-  }
-  
+  ikSolver.assemble( controller.state ); //std::cout << "OSim: IK solver assembled" << std::endl;
+  ikSolver.track( controller.state );
+  // Acquire resulting joint setpoints
   for( size_t jointIndex = 0; jointIndex < controller.actuatorsList.size(); jointIndex++ )
   {
     OpenSim::Coordinate* jointCoordinate = controller.actuatorsList[ jointIndex ]->getCoordinate();
-    jointSetpointsList[ jointIndex ]->position = jointCoordinate->getValue( controller.stateFD );
+    jointSetpointsList[ jointIndex ]->position = jointCoordinate->getValue( controller.state );
+    jointSetpointsList[ jointIndex ]->velocity = jointCoordinate->getSpeedValue( controller.state );
+    jointSetpointsList[ jointIndex ]->acceleration = jointCoordinate->getAccelerationValue( controller.state );
+    double controlAction = jointSetpointsList[ jointIndex ]->position - jointMeasuresList[ jointIndex ]->position;
     size_t actuatorOutputsIndex = jointIndex * EMG_OUTPUT_VARS_NUMBER;
-    jointSetpointsList[ jointIndex ]->force = actuatorOutputs[ actuatorOutputsIndex + EMG_TORQUE_INT ];
+    jointSetpointsList[ jointIndex ]->force = controlAction - actuatorOutputs[ actuatorOutputsIndex + EMG_TORQUE_INT ];
   }
 }
